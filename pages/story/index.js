4a1219f91526cba516fb0ea4c0800f68a0d17904
SRAKIB17 <rakibulssc5@gmail.com>
2022-06-21 22:38:18 +0600

    d

--- pages/story/index.js
+++ pages/story/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import PostBody from '../../components/Post-NewsFeed/PostBody';
 
 const index = () => {
     return (
         <div>
-            <PostBody/>
         </div>
     );
 };
