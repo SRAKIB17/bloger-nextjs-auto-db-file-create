3e4a629633056151e80413e6df66682a8f25a592
SRAKIB17 <rakibulssc5@gmail.com>
2022-06-24 03:33:50 +0600

    ok

--- components/Header/ProfilePicture.js
+++ components/Header/ProfilePicture.js
@@ -13,7 +13,7 @@ const ProfilePicture = () => {
                 <ul className="menu menu-horizontal p-0 flex items-center md:absolute md:right-0 md:top-0">
 
                     <li>
-                        <button onMouseEnter={ProfilePictureHandle} >
+                        <button onMouseEnter={ProfilePictureHandle} onClick={ProfilePictureHandle}>
                             <div className="avatar ">
                                 <div className="w-8 rounded-full ring ring-inherit ring-offset-base-100 ring-offset-1">
                                     <img src="https://api.lorem.space/image/face?hash=3174" alt='' />
@@ -24,7 +24,7 @@ const ProfilePicture = () => {
 
                 </ul>
             </div>
-            <div className='border border-double onClickProfileMenu right-0 bg-base-100 mt-[-100px] rounded-b-lg shadow-2xl' id='profileLogOut'>
+            <div className='border border-double onClickProfileMenu right-0 bg-base-100 mt-[-1000px] rounded-b-lg shadow-2xl' id='profileLogOut'>
                 <div className='p-3'>
                     <ul className="menu bg-base-100 w-56 rounded-box">
                         <li className="hover-bordered"><a>Item 1</a></li>
