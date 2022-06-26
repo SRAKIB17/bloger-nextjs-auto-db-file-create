e9e277e2051855ecd8c6f4587091ab1287fcbce3
SRAKIB17 <rakibulssc5@gmail.com>
2022-06-26 21:53:31 +0600

    ok

--- pages/videos/index.js
+++ pages/videos/index.js
@@ -18,18 +18,17 @@ const Index = () => {
         <div>
             <Header />
             <div className='grid grid-cols-12 gap-3'>
-                <div className='hidden sm:block sm:col-span-5 md:col-span-4 text-justify lg:ml-16 p-1 relative'>
-
-                    <div className='fixed h-[100vh] overflow-auto w-[250px]'>
+                <div className='hidden sm:block sm:col-span-4 md:col-span-4 text-justify lg:ml-16 p-1 relative bg-base-100'>
+                    <div className='fixed h-[100vh] overflow-auto sm:w-[200px] md:max-w-[300px] lg:max-w-[350px] md:w-full'>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, cumque adipisci nisi possimus nostrum porro atque temporibus rerum repellat. Maxime, cupiditate! Fuga nobis velit animi voluptatem quam quo ea rerum!
                     </div>
                 </div>
 
-                <div className='col-span-12 sm:mr-3 sm:col-start-6 sm:col-end-[-1] md:col-span-5'>
+                <div className='col-span-12 sm:mr-3 sm:col-start-5 sm:col-end-[-1] md:col-span-7 lg:col-span-6'>
                     <Post posts={post} />
                 </div>
 
-                <div className=' col-span-3 hidden md:block relative'>
+                <div className=' col-span-2 hidden lg:block relative'>
                     <div className='fixed h-full overflow-auto text-justify pr-3'>
                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium omnis ipsam repellat hic soluta explicabo consequuntur? Sed nobis dolore sit, minus illum ipsam laboriosam neque, voluptatem, deleniti voluptate amet praesentium?
                         <RightMenu />
