6a5e0db85eaff05b0971e99bc702c3999657b5ab
SRAKIB17 <rakibulssc5@gmail.com>
2022-06-30 04:11:43 +0600

    ok

--- pages/story/index.js
+++ pages/story/index.js
@@ -56,17 +56,17 @@ const Index = () => {
     return (
         <div>
             <Header />
-            <div className='grid grid-cols-12 gap-2 bg-base-100'>
+            <div className='grid grid-cols-12 gap-2'>
                 <div className='hidden sm:block sm:col-span-4 md:col-span-4 text-justify lg:ml-16 p-1 relative bg-base-100'>
                     <div className='fixed h-[100vh] overflow-auto sm:w-[200px] md:max-w-[300px] lg:max-w-[350px] md:w-full'>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, cumque adipisci nisi possimus nostrum porro atque temporibus rerum repellat. Maxime, cupiditate! Fuga nobis velit animi voluptatem quam quo ea rerum!
                     </div>
                 </div>
 
-                <div className='col-span-12 sm:mr-3 sm:col-start-5 sm:col-end-[-1] md:col-span-8 lg:col-span-5' id='storyScroll'>
+                <div className='col-span-12 sm:mr-3 md:mr-0 sm:col-start-5 sm:col-end-[-1] md:col-span-8 lg:col-span-5' id='storyScroll'>
 
                     <Post posts={getPost} refetch={refetch} />
-                    
+
                     {
                         isLoading ||
                         <div className=" p-4 text-center">
