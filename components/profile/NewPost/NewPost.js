01ae0ee38da0d48a4419c8ab89fa6b322695ca0a
SRAKIB17 <rakibulssc5@gmail.com>
2022-06-30 04:15:28 +0600

    new post

--- components/profile/NewPost/NewPost.js
+++ components/profile/NewPost/NewPost.js
@@ -58,7 +58,7 @@ const NewPost = () => {
             const { data } = await axios.post('/api/post/newpost', post);
             console.log(data)
             if (data?.result?.acknowledged) {
-                // event.target.reset()
+                event.target.reset()
             }
             // console.log(data)
 
