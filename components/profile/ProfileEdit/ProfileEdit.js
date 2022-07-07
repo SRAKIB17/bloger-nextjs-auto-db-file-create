7d9cd9d954fc6f3df2c5c15a6383cdb1f3cfe5fe
SRAKIB17 <rakibulssc5@gmail.com>
2022-07-07 19:19:46 +0600

    ok

--- components/profile/ProfileEdit/ProfileEdit.js
+++ components/profile/ProfileEdit/ProfileEdit.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from '../NewPost/NewPost.module.css'
+import { FacebookSquare, Github, Youtube, LinkedinNew, Instagram, Twitter, TwitterSquare, Quote, Gender, School, Location, Worker } from '../../ReactRSIcon/index';
 
 const ProfileEdit = () => {
     function closeNewPost() {
@@ -18,35 +19,166 @@ const ProfileEdit = () => {
     }
     return (
         <div>
-            <div id="profileEdit" className={styles.NewPostNav}>
+            <div id="profileEdit" className={styles.NewPostNav + ' bg-base-100'}>
                 <a href="#" className={styles.closebtn} onClick={closeNewPost}>&times;</a>
                 <div>
                     <form action="" onSubmit={postHandle} className='flex flex-col gap-2 m-10'>
 
-                        <input
-                            type="text"
-                            name="title"
-                            id=""
-                            className='input input-success form-control w-56 sm:w-80'
-                            placeholder='Title'
-                        />
 
-                        <input
-                            type="text"
-                            name="title"
-                            id=""
-                            className='input input-success form-control w-56 sm:w-80'
-                            placeholder='Short description'
-                        />
-                        <input
-                            type="text"
-                            name="title"
-                            id=""
-                            className='input input-success form-control w-56 sm:w-80'
-                            placeholder='Category'
-                        />
-                        
-                        <input type="submit" value="Post" className='btn rounded-3xl btn-primary text-white w-fit' />
+                        <div className='p-1'>
+                            <table className='table-auto'>
+                                <tbody className='flex flex-col gap-2'>
+                                    <tr>
+                                        <td className='flex items-center gap-1'>
+                                            <Worker />
+                                            <input
+                                                type="text"
+                                                name="title"
+                                                id=""
+                                                className='input input-sm input-primary form-control w-56 sm:w-80'
+                                                placeholder='Title'
+                                            />
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className='flex items-center gap-1'>
+                                            <Location />
+                                            <input
+                                                type="text"
+                                                name="title"
+                                                id=""
+                                                className='input input-sm input-primary form-control w-56 sm:w-80'
+                                                placeholder='Title'
+                                            />
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className='flex items-center gap-1'>
+                                            <School />
+                                            <input
+                                                type="text"
+                                                name="title"
+                                                id=""
+                                                className='input input-sm input-primary form-control w-56 sm:w-80'
+                                                placeholder='Title'
+                                            />
+                                        </td>
+                                    </tr>
+                                    {/* <tr>
+                    <td><img src={relationship} alt="" /> Single</td>
+                </tr> */}
+                                    {/* <tr>
+                    <td><img src={birthday} alt="" /> Birthday</td>
+                </tr> */}
+                                    <tr>
+                                        <td className='flex items-center gap-1'>
+                                            <Github />
+                                            <input
+                                                type="text"
+                                                name="title"
+                                                id=""
+                                                className='input input-sm input-primary form-control w-56 sm:w-80'
+                                                placeholder='Title'
+                                            />
+                                        </td>
+                                    </tr>
+
+                                    <tr>
+                                        <td className='flex items-center gap-1'>
+                                            <Youtube />
+                                            <input
+                                                type="text"
+                                                name="title"
+                                                id=""
+                                                className='input input-sm input-primary form-control w-56 sm:w-80'
+                                                placeholder='Title'
+                                            />
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className='flex items-center gap-1'>
+                                            <FacebookSquare className='rounded-sm' />
+                                            <input
+                                                type="text"
+                                                name="title"
+                                                id=""
+                                                className='input input-sm input-primary form-control w-56 sm:w-80'
+                                                placeholder='Title'
+                                            />
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className='flex items-center gap-1'>
+                                            <LinkedinNew />
+                                            <input
+                                                type="text"
+                                                name="title"
+                                                id=""
+                                                className='input input-sm input-primary form-control w-56 sm:w-80'
+                                                placeholder='Title'
+                                            />
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className='flex items-center gap-1'>
+                                            <Instagram />
+                                            <input
+                                                type="text"
+                                                name="title"
+                                                id=""
+                                                className='input input-sm input-primary form-control w-56 sm:w-80'
+                                                placeholder='Title'
+                                            />
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className='flex items-center gap-1'>
+                                            <Twitter />
+                                            <input
+                                                type="text"
+                                                name="title"
+                                                id=""
+                                                className='input input-sm input-primary form-control w-56 sm:w-80'
+                                                placeholder='Title'
+                                            />
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className='flex items-center gap-1'>
+                                            <Gender />
+                                            <select className="select select-primary select-sm select-bordered select-md w-full max-w-xs">
+                                                <option disabled selected>Gender</option>
+                                                <option value='Male'>Male</option>
+                                                <option value='Female'>Female</option>
+                                            </select>
+                                        </td>
+                                    </tr>
+                                    {/* <tr>
+                    <td> <img src={religion} alt="" /> riligion</td>
+                </tr> */}
+                                    <tr>
+                                        <td className='flex items-center gap-1'>
+                                            <Quote />
+                                            <input
+                                                type="text"
+                                                name="title"
+                                                id=""
+                                                className='input input-sm input-primary form-control w-56 sm:w-80'
+                                                placeholder='Title'
+                                            />
+                                        </td>
+
+                                    </tr>
+                                    <tr>
+                                        <td className='flex items-center gap-1'>
+                                            <input type="submit" value="Post" className='btn btn-sm rounded-3xl btn-primary text-white w-fit' />
+                                        </td>
+
+                                    </tr>
+                                </tbody>
+                            </table>
+
+                        </div>
                     </form>
 
                 </div>
