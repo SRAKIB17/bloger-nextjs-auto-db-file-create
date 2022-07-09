326687893f58e3fad78020a27225ce996d664281
SRAKIB17 <rakibulssc5@gmail.com>
2022-07-09 18:03:56 +0600

    send message

--- components/SupportInbox/SupportInbox.js
+++ components/SupportInbox/SupportInbox.js
@@ -29,7 +29,6 @@ const SupportInbox = () => {
     }
     const handleChatBoxHeight = () => {
         const sendMessageSupportInboxForm = document.getElementById('sendMessageSupportInboxForm');
-
         document.getElementById('supportMessageBody').style.height = document.getElementById('SupportInbox').offsetHeight - (sendMessageSupportInboxForm.offsetHeight + 80) + 'px'
     }
     useEffect(() => {
@@ -40,8 +39,6 @@ const SupportInbox = () => {
     }, [])
     const heightAutoHandle = (e) => {
         handleChatBoxHeight()
-
-
         e.target.style.height = 'auto';
         if (e.target.scrollHeight < 150) {
             e.target.style.height = e.target.scrollHeight + 'px'
@@ -53,108 +50,12 @@ const SupportInbox = () => {
 
     // const { data, refetch } = useQuery(['supportMessage', 54354], () => axios.get('/api/comment'))
     // const message = data?.data
-    const message = [
-        {
-            userID: '2',
-            adminReply: false,
-            adminId: '534fsdf545534',
-            message: '543fsf5645',
-            support_id: 54435345
-        },
-        {
-            userID: '1',
-            support_id: 54465435345
-            ,
-            adminReply: true,
-            adminId: '5345fsdfds45534',
-            message: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quam quis amet enim qui illo facilis corrupti libero molestias commodi itaque voluptate molestiae, ab ducimus? Quis perspiciatis quam quod voluptas?'
-        },
-        {
-            userID: '1',
-            adminReply: false,
-            adminId: '534545534',
-            message: 'গিয়েছিলাম খিচুড়ি নিয়ে, সবাই আনন্দ করে খাইলো কিন্তু ৫০ টির মতো হিন্দু ধর্মাবলম্বী পরিবার খিচুড়ি গ্রহন কর রেনি। কারণ তাদের ধর্মের বিধানানুযায়ী আজকের জন্য তা গ্রহন করতে নিষেধ করা হয়েছে। তাই তড়িঘড়ি করে এ ৫০ টি পরিবারের জন্য ফল-ফ্রুটসের ব্যবস্থা করলাম। আহা তাদের খুশি কে দেখে! কিছু কিছু সময় নিজেকে বিলিয়ে দিতে ইচ্ছে করে।',
-            support_id: 54435345454445
 
-        },
-        {
-            userID: '1',
-            adminReply: false,
-            adminId: '534545534',
-            message: 'গিয়েছিলাম খিচুড়ি নিয়ে, সবাই আনন্দ করে খাইলো কিন্তু ৫০ টির মতো হিন্দু ধর্মাবলম্বী পরিবার খিচুড়ি গ্রহন কর রেনি। কারণ তাদের ধর্মের বিধানানুযায়ী আজকের জন্য তা গ্রহন করতে নিষেধ করা হয়েছে। তাই তড়িঘড়ি করে এ ৫০ টি পরিবারের জন্য ফল-ফ্রুটসের ব্যবস্থা করলাম। আহা তাদের খুশি কে দেখে! কিছু কিছু সময় নিজেকে বিলিয়ে দিতে ইচ্ছে করে।',
-            support_id: 54435345454445
-
-        },
-        {
-            userID: '1',
-            adminReply: false,
-            adminId: '534545534',
-            message: 'গিয়েছিলাম খিচুড়ি নিয়ে, সবাই আনন্দ করে খাইলো কিন্তু ৫০ টির মতো হিন্দু ধর্মাবলম্বী পরিবার খিচুড়ি গ্রহন কর রেনি। কারণ তাদের ধর্মের বিধানানুযায়ী আজকের জন্য তা গ্রহন করতে নিষেধ করা হয়েছে। তাই তড়িঘড়ি করে এ ৫০ টি পরিবারের জন্য ফল-ফ্রুটসের ব্যবস্থা করলাম। আহা তাদের খুশি কে দেখে! কিছু কিছু সময় নিজেকে বিলিয়ে দিতে ইচ্ছে করে।',
-            support_id: 54435345454445
-
-        },
-        {
-            userID: '1',
-            adminReply: true,
-            adminId: '534545534',
-            message: `'গিয়েছিলাম খিচুড়ি নিয়ে, সবাই আনন্দ করে খাইলো কিন্তু ৫০ টির মতো হিন্দু ধর্মাবলম্বী পরিবার খিচুড়ি গ্রহন কর রেনি। কারণ তাদের ধর্মের বিধানানুযায়ী আজকের জন্য তা গ্রহন করতে নিষেধ করা হয়েছে। তাই তড়িঘড়ি করে এ ৫০ টি পরিবারের জন্য ফল-ফ্রুটসের ব্যবস্থা করলাম। আহা তাদের খুশি কে দেখে! কিছু কিছু সময় নিজেকে বিলিয়ে দিতে ইচ্ছে করে।
-            <video width="320" height="240"  preload="auto" controls muted poster="http://rakib17.hexat.com/icon/busy.gif" loop autoplay>
-                <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4">
-                <source src="movie.ogg" type="video/ogg">
-                Your browser does not support the video tag.
-            </video>
-    
-            `,
-            support_id: 54435345454445
-
-        },
-        {
-            userID: '1',
-            adminReply: false,
-            adminId: '534545534',
-            message: 'গিয়েছিলাম খিচুড়ি নিয়ে, সবাই আনন্দ করে খাইলো কিন্তু ৫০ টির মতো হিন্দু ধর্মাবলম্বী পরিবার খিচুড়ি গ্রহন কর রেনি। কারণ তাদের ধর্মের বিধানানুযায়ী আজকের জন্য তা গ্রহন করতে নিষেধ করা হয়েছে। তাই তড়িঘড়ি করে এ ৫০ টি পরিবারের জন্য ফল-ফ্রুটসের ব্যবস্থা করলাম। আহা তাদের খুশি কে দেখে! কিছু কিছু সময় নিজেকে বিলিয়ে দিতে ইচ্ছে করে।',
-            support_id: 54435345454445
-
-        },
-        {
-            userID: '4',
-            adminReply: false,
-            adminId: '534545534',
-            message: `
-            <video width="320" height="240"  preload="auto" controls muted poster="http://rakib17.hexat.com/icon/busy.gif" loop autoplay>
-                <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4">
-                <source src="movie.ogg" type="video/ogg">
-                Your browser does not support the video tag.
-            </video>
-    
-            `,
-            support_id: 54435566345
-        },
-
-        {
-            userID: '4',
-            adminReply: true,
-            adminId: '534545534',
-            message: `
-            <video width="320" height="240"  preload="auto" controls muted poster="http://rakib17.hexat.com/icon/busy.gif" loop autoplay>
-                <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4">
-                <source src="movie.ogg" type="video/ogg">
-                Your browser does not support the video tag.
-            </video>
-    
-            `,
-            support_id: 66546
-
-        }
-    ]
     const [inboxMessage, setInboxMessage] = useState([]);
     const [inboxUserId, setInboxUserId] = useState(1);
-    useEffect(() => {
-        const getInboxMessage = message.filter(inbox => inbox.userID == inboxUserId);
-        setInboxMessage(getInboxMessage)
-    }, [inboxUserId])
 
-
+    const { data } = useQuery(['SupportInbox',inboxUserId], () => axios.get(`/api/support_inbox/${inboxUserId}`))
+    console.log()
 
     const postHandle = async (event) => {
         event.preventDefault();
@@ -271,7 +172,7 @@ const SupportInbox = () => {
 
                         {
                             admin?.admin &&
-                            <div className={style.showAllMessage + ' overflow-hidden border-b-2 border-b-primary'} id='adminAllInboxMessage'>
+                            <div className={style.showAllMessage + ' overflow-hidden  border-b-primary'} id='adminAllInboxMessage'>
                                 <div className='rounded-lg border-b overflow-auto h-56 hideScrollBar'>
                                     <AdminSupportInbox setInboxMessage={{ setInboxMessage, inboxUserId, setInboxUserId }} />
                                 </div>
@@ -282,7 +183,7 @@ const SupportInbox = () => {
                         <div>
 
                             {
-                                inboxMessage?.map((messageBody, index) => <MessageBody key={messageBody?.support_id} messageBody={messageBody} />)
+                                data?.data?.map((messageBody, index) => <MessageBody key={messageBody?.support_id} messageBody={messageBody} />)
                             }
                         </div>
 
---
+++ pages/api/support_inbox/[id].js
@@ -0,0 +1,107 @@
+
+const message = [
+    {
+        userID: '2',
+        adminReply: false,
+        adminId: '534fsdf545534',
+        message: '543fsf5645',
+        support_id: 54435345
+    },
+    {
+        userID: '1',
+        support_id: 54465435345
+        ,
+        adminReply: true,
+        adminId: '5345fsdfds45534',
+        message: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quam quis amet enim qui illo facilis corrupti libero molestias commodi itaque voluptate molestiae, ab ducimus? Quis perspiciatis quam quod voluptas?'
+    },
+    {
+        userID: '1',
+        adminReply: false,
+        adminId: '534545534',
+        message: 'গিয়েছিলাম খিচুড়ি নিয়ে, সবাই আনন্দ করে খাইলো কিন্তু ৫০ টির মতো হিন্দু ধর্মাবলম্বী পরিবার খিচুড়ি গ্রহন কর রেনি। কারণ তাদের ধর্মের বিধানানুযায়ী আজকের জন্য তা গ্রহন করতে নিষেধ করা হয়েছে। তাই তড়িঘড়ি করে এ ৫০ টি পরিবারের জন্য ফল-ফ্রুটসের ব্যবস্থা করলাম। আহা তাদের খুশি কে দেখে! কিছু কিছু সময় নিজেকে বিলিয়ে দিতে ইচ্ছে করে।',
+        support_id: 54435345454445
+
+    },
+    {
+        userID: '1',
+        adminReply: false,
+        adminId: '534545534',
+        message: 'গিয়েছিলাম খিচুড়ি নিয়ে, সবাই আনন্দ করে খাইলো কিন্তু ৫০ টির মতো হিন্দু ধর্মাবলম্বী পরিবার খিচুড়ি গ্রহন কর রেনি। কারণ তাদের ধর্মের বিধানানুযায়ী আজকের জন্য তা গ্রহন করতে নিষেধ করা হয়েছে। তাই তড়িঘড়ি করে এ ৫০ টি পরিবারের জন্য ফল-ফ্রুটসের ব্যবস্থা করলাম। আহা তাদের খুশি কে দেখে! কিছু কিছু সময় নিজেকে বিলিয়ে দিতে ইচ্ছে করে।',
+        support_id: 54435345454445
+
+    },
+    {
+        userID: '1',
+        adminReply: false,
+        adminId: '534545534',
+        message: 'গিয়েছিলাম খিচুড়ি নিয়ে, সবাই আনন্দ করে খাইলো কিন্তু ৫০ টির মতো হিন্দু ধর্মাবলম্বী পরিবার খিচুড়ি গ্রহন কর রেনি। কারণ তাদের ধর্মের বিধানানুযায়ী আজকের জন্য তা গ্রহন করতে নিষেধ করা হয়েছে। তাই তড়িঘড়ি করে এ ৫০ টি পরিবারের জন্য ফল-ফ্রুটসের ব্যবস্থা করলাম। আহা তাদের খুশি কে দেখে! কিছু কিছু সময় নিজেকে বিলিয়ে দিতে ইচ্ছে করে।',
+        support_id: 54435345454445
+
+    },
+    {
+        userID: '1',
+        adminReply: true,
+        adminId: '534545534',
+        message: `'গিয়েছিলাম খিচুড়ি নিয়ে, সবাই আনন্দ করে খাইলো কিন্তু ৫০ টির মতো হিন্দু ধর্মাবলম্বী পরিবার খিচুড়ি গ্রহন কর রেনি। কারণ তাদের ধর্মের বিধানানুযায়ী আজকের জন্য তা গ্রহন করতে নিষেধ করা হয়েছে। তাই তড়িঘড়ি করে এ ৫০ টি পরিবারের জন্য ফল-ফ্রুটসের ব্যবস্থা করলাম। আহা তাদের খুশি কে দেখে! কিছু কিছু সময় নিজেকে বিলিয়ে দিতে ইচ্ছে করে।
+        <video width="320" height="240"  preload="auto" controls muted poster="http://rakib17.hexat.com/icon/busy.gif" loop autoplay>
+            <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4">
+            <source src="movie.ogg" type="video/ogg">
+            Your browser does not support the video tag.
+        </video>
+
+        `,
+        support_id: 54435345454445
+
+    },
+    {
+        userID: '1',
+        adminReply: false,
+        adminId: '534545534',
+        message: 'গিয়েছিলাম খিচুড়ি নিয়ে, সবাই আনন্দ করে খাইলো কিন্তু ৫০ টির মতো হিন্দু ধর্মাবলম্বী পরিবার খিচুড়ি গ্রহন কর রেনি। কারণ তাদের ধর্মের বিধানানুযায়ী আজকের জন্য তা গ্রহন করতে নিষেধ করা হয়েছে। তাই তড়িঘড়ি করে এ ৫০ টি পরিবারের জন্য ফল-ফ্রুটসের ব্যবস্থা করলাম। আহা তাদের খুশি কে দেখে! কিছু কিছু সময় নিজেকে বিলিয়ে দিতে ইচ্ছে করে।',
+        support_id: 54435345454445
+
+    },
+    {
+        userID: '4',
+        adminReply: false,
+        adminId: '534545534',
+        message: `
+        <video width="320" height="240"  preload="auto" controls muted poster="http://rakib17.hexat.com/icon/busy.gif" loop autoplay>
+            <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4">
+            <source src="movie.ogg" type="video/ogg">
+            Your browser does not support the video tag.
+        </video>
+
+        `,
+        support_id: 54435566345
+    },
+
+    {
+        userID: '4',
+        adminReply: true,
+        adminId: '534545534',
+        message: `
+        <video width="320" height="240"  preload="auto" controls muted poster="http://rakib17.hexat.com/icon/busy.gif" loop autoplay>
+            <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4">
+            <source src="movie.ogg" type="video/ogg">
+            Your browser does not support the video tag.
+        </video>
+
+        `,
+        support_id: 66546
+
+    }
+]
+export default function handler(req, res) {
+    const { id } = req.query;
+    const getInboxMessage = message.filter(inbox => inbox.userID == id);
+    const method = req.method;
+    if (method === "GET") {
+        res.status(200).json(getInboxMessage)
+    }
+    // if (method === 'POST') {
+    //     message.push(req.body)
+    // }
+
+}
