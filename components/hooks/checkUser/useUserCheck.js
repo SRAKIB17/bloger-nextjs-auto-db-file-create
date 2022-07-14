22b3698ff7dbcee0eebdea75aeb35ce44b3fd683
SRAKIB17 <rakibulssc5@gmail.com>
2022-07-14 14:08:05 +0600

    ok

--- components/hooks/checkUser/useUserCheck.js
+++ components/hooks/checkUser/useUserCheck.js
@@ -24,47 +24,52 @@ const useUserCheck = () => {
         const run = async () => {
             setIsLoading(true)
             //GET USER EMAIL JWT TOKEN FROM LOCALSTORAGE
-            const token = localStorage.getItem('token');
-            const email = jwtDecode(token)?.jwtInfo?.email;
+            try {
+                const token = localStorage.getItem('token');
+                const email = jwtDecode(token)?.jwtInfo?.email;
 
-            //GET USER PASS CODE GET FROM COOKIES AND SPLIT , FIND PASS CODE
-            const login_info = document.cookie.split(';').find(token => token.includes('login'))?.split('=')?.[1]
-            const login_pass = jwtDecode(login_info)?.userInfo
+                //GET USER PASS CODE GET FROM COOKIES AND SPLIT , FIND PASS CODE
+                const login_info = document.cookie.split(';').find(token => token.includes('login'))?.split('=')?.[1]
+                const login_pass = jwtDecode(login_info)?.userInfo
 
-            // IF EMAIL AND LOGIN_INFO GET THEN THIS CONDITION IS TRUE
-            if (email && login_pass) {
-                const password = login_pass?.token;
-                const userId = login_pass?.userId;
+                // IF EMAIL AND LOGIN_INFO GET THEN THIS CONDITION IS TRUE
+                if (email && login_pass) {
+                    const password = login_pass?.token;
+                    const userId = login_pass?.userId;
 
-                // FORM SEND BACKEND API BY POST METHOD
-                const form = {
-                    email: email,
-                    password: password,
-                    userId: userId
-                }
-
-                const { data } = await axios.post('/api/login_signup/check_user_auto_login', form, {
-                    headers: {
-                        'login_api_code': `dcab4733a9ce28bbb1a7a66d80a4097b`
+                    // FORM SEND BACKEND API BY POST METHOD
+                    const form = {
+                        email: email,
+                        password: password,
+                        userId: userId
                     }
-                });
-         
-                //IF SUCCESS SET_USER TRUE
-                if (data?.success) {
-                    setUserInfo(data?.user_details)
-                    setUser({ user: true });
 
+                    const { data } = await axios.post('/api/login_signup/check_user_auto_login', form, {
+                        headers: {
+                            'login_api_code': `dcab4733a9ce28bbb1a7a66d80a4097b`
+                        }
+                    });
+
+                    //IF SUCCESS SET_USER TRUE
+                    if (data?.success) {
+                        setUserInfo(data?.user_details)
+                        setUser({ user: true });
+
+                    }
+
+                    //IF FAILED SET_USER TRUE
+                    else {
+                        setUser({ user: false });
+                    }
                 }
-
-                //IF FAILED SET_USER TRUE
                 else {
                     setUser({ user: false });
                 }
+                setIsLoading(null)
             }
-            else {
-                setUser({ user: false });
+            catch {
+                setIsLoading(null)
             }
-            setIsLoading(null)
         }
         run().catch(console.dir)
         // console.log(login_info)
