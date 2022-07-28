e7b5e807ba918aade469eedc118676df9dc631d5
SRAKIB17 <rakibulssc5@gmail.com>
2022-07-28 22:06:22 +0600

    print

--- pages/print/[id].js
+++ pages/print/[id].js
@@ -1,25 +1,113 @@
+/* eslint-disable @next/next/no-img-element */
+import axios from 'axios';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useState } from 'react';
+import { useQuery } from 'react-query';
+import autoJwtTokenGenerateForUserOrGuest from '../../components/hooks/autoJwtTokenGenerateForUserOrGuest';
+import { Camera } from '../../components/ReactRSIcon';
+
+import coverPhoto1 from '../../public/print/print-cove-1.jpg'
+import coverPhoto2 from '../../public/print/print-cove-2.jpg'
 
 const Index = () => {
     const router = useRouter()
     const { id } = router.query;
+    autoJwtTokenGenerateForUserOrGuest()
+
+    const { data, refetch, isLoading } = useQuery(['print-story', id], () => axios.get(`/api/post/find-specific-story?post_id=${id}`,
+        {
+            headers: { access_token: sessionStorage.getItem('accessAutoG') }
+        }
+    ))
+    const post = data?.data || []
+    const userID = data?.data?.userID
+    const userInfo = useQuery(['printPostUserDetails', userID], () => axios.get(`/api/public_user_details/${userID}`,
+        {
+            headers: { access_token: sessionStorage.getItem('accessAutoG') }
+        }));
+    const user_details = userInfo?.data?.data?.user_details;
 
     const onloadIframeHeightStylesHandle = async (id) => {
         const iframe = document.getElementById('print' + id)
         iframe.style.height = await iframe.contentWindow.document.documentElement.scrollHeight + 'px';
 
-        setTimeout(() => {
-            print()
-
-        }, 2000);
     }
 
 
+    const copyLink = `${typeof window !== 'undefined' && window.location.origin ? window.location.origin : ''}/story/${id}`;
+    const { thumbnail, postRefMode, short_description } = post;
+
+    // const coverPhoto = [
+    //     {
+    //         pic: coverPhoto1?.src
+    //     }
+    // ]
+    // badge[Math.floor(Math.random() * badge.length)]
+
+    let count = 10;
+    const [countT, setCountT] = useState(10)
+    const countTime = setInterval(() => {
+        if (count < 0) {
+            document.getElementById('count').style.display = 'none'
+            clearInterval(countTime);
+            print()
+        }
+        setCountT(count);
+        count--
+
+    }, 1000);
     return (
         <div>
-            <div className='text-5xl text-primary text-center pt-10'>
-                ProgLearn
+            <div className='border-b-4 border-red-200'>
+                <div className='fixed top-[50%] left-[50%]'>
+                    <p className='btn btn-disabled bg-secondary text-white font-mono font-light' id='count'>
+                        Please Wait {countT + ' '}sec
+                    </p>
+                </div>
+                <div className='p-5 pl-10 pr-10'>
+                    <div className='font-mono text-center font-extrabold'>
+                        <p className='text-6xl border-b-2 border-primary'>{post?.post_title}</p>
+                        <p>--{user_details?.name}</p>
+                    </div>
+
+                    <div className='pt-20 mb-20'>
+                        <div className='float-left'>
+                            {/* ----thumbnail------------ */}
+                            {
+                                (postRefMode === 'text' && thumbnail) ?
+                                    <div className='mr-4'>
+                                        <figure>
+                                            <img
+                                                src={thumbnail}
+                                                alt=""
+                                                className='max-w-[200px] h-[200px] md:max-w-[250px] md:h-[160px] rounded-md border-2'
+                                            />
+                                        </figure>
+                                    </div>
+                                    :
+                                    <div className='mr-4'>
+                                        <Camera size='150' className="border-2 rounded-md" />
+                                    </div>
+                            }
+                        </div>
+                        {/* style={{ width: '100%', wordWrap: "break-word", whiteSpace: 'pre-line' }} */}
+                        <div className='text-justify'>
+                            {
+                                short_description?.slice(0, 1000)
+                            }
+                        </div>
+                    </div>
+                    <div className='font-mono'>
+                        <p className='text-2xl'>
+                            ProgLearn
+                        </p>
+                        <p className='text-2xs'>
+                            <a href={{ copyLink }} className='text-primary'>
+                                See Post
+                            </a>
+                        </p>
+                    </div>
+                </div>
             </div>
             <iframe
                 src={"/api/preview/" + id}
