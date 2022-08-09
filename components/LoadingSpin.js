a4808a8fd50c4344817125742cc14e3bec26a8aa
SRAKIB17 <rakibulssc5@gmail.com>
2022-08-09 21:29:41 +0600

    done loading spin

--- components/LoadingSpin.js
+++ components/LoadingSpin.js
@@ -4,7 +4,7 @@ import Header from './Header/Header';
 const LoadingSpin = () => {
     return (
         <div>
-            <div className='flex justify-center mt-8 mb-8 overflow-hidden'>
+            <div className='flex justify-center pt-8 mb-8 overflow-hidden'>
                 <div className='animate-spin text-center border-r-4 border-l-4 w-28 h-28 rounded-[50%] border-red-200'>
                 </div>
             </div>
