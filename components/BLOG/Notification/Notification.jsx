e145d52b9cb51b3af922ba634d4c66b9b9f04d64
SRAKIB17 <rakibulssc5@gmail.com>
2022-09-19 14:48:09 +0600

    'done'

--- components/BLOG/Notification/Notification.jsx
+++ components/BLOG/Notification/Notification.jsx
@@ -1,8 +1,9 @@
 import axios from 'axios';
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { useQuery } from 'react-query';
 import { UserFullInfoProvider } from '../../../pages/_app';
 import LoadingSpin from '../../LoadingSpin';
+import { Delete } from '../../ReactRSIcon';
 import MapNotificationView from './MapNotificationView';
 
 const Notification = () => {
@@ -19,10 +20,65 @@ const Notification = () => {
         }
     ))
     const notifications = data?.data;
-    2
+
+    const [clear_notificationLoading, setNotificationClear] = useState(false)
+
+    const deleteAllNotificationHandle = async () => {
+        setNotificationClear(true)
+        const data = await axios.delete(`/api/notification/clear_notification?user_id=${user_details?.userID}&email=${user_details?.email}`,
+            {
+                headers: {
+                    access_token: sessionStorage.getItem('accessAutoG'),
+                    token: localStorage.getItem('token')
+                }
+            }
+        )
+        refetch()
+        const result = data?.data
+        if (result?.acknowledged) {
+            refetch()
+            setNotificationClear(false)
+        }
+        else {
+            alert('something is wrong')
+        }
+        setNotificationClear(false)
+    }
     return (
         <div className='max-w-3xl mx-auto'>
             <div className='flex flex-col gap-3 mt-5 text-primary'>
+                <div className='flex justify-end'>
+                    {
+                        clear_notificationLoading ?
+                            <span
+
+                                className='btn cursor-pointer text-red-600 flex gap-1 items-center btn-ghost capitalize btn-sm'
+                            >
+                                <span>
+                                    <Delete />
+                                </span>
+                                <span>
+                                    Clear Notification
+                                </span>
+                                <span className='absolute'>
+                                    <p className='border-r-2 border-b-2 border-primary h-5 w-5 rounded-full animate-spin'>
+
+                                    </p>
+                                </span>
+                            </span>
+                            :
+                            <button
+                                onClick={() => deleteAllNotificationHandle()}
+                                className='btn text-red-600 flex gap-1 items-center btn-ghost capitalize btn-sm'
+                            >
+                                <span>
+                                    <Delete />
+                                </span>
+                                <span>
+                                    Clear Notification
+                                </span>
+                            </button>}
+                </div>
                 {
                     isLoading ?
                         <div className='mt-4'>
--- pages/api/notification/clear_notification.js
+++ pages/api/notification/clear_notification.js
@@ -1,4 +1,4 @@
-import { ObjectId } from "mongodb";
+
 import login_user_without_post_body from "../../../components/hooks/api/social/login_user_without_post_body";
 import verifyUserAndAccessFeatureServer from "../../../components/hooks/api/verifyUser/verifyUserAndAccessFeatureServer";
 
@@ -7,11 +7,11 @@ export default async function handler(req, res) {
     await client.connect();
 
     const checkUser = await verifyUserAndAccessFeatureServer(req);
-    const { id } = req.query
+    const { user_id } = (req.query)
     if (checkUser) {
         // const { user_id } = req.query
         const notificationCollection = client.db("Notifications").collection("Notification");
-        return res.status(200).json(await notificationCollection.deleteOne({ _id: ObjectId(id) }));
+        return res.status(200).json(await notificationCollection.deleteMany({ userID: user_id }));
     }
     else {
         return res.status(200).json({})
