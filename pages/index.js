8c3de5a1726f44156cb55067978e9c2beb6f4267
SRAKIB17 <rakibulssc5@gmail.com>
2022-09-21 15:52:39 +0600

    'done

--- pages/index.js
+++ pages/index.js
@@ -16,70 +16,7 @@ export default function Index() {
     router.push(router)
     router.prefetch(router)
   }
-  useEffect(() => {
-    const words = ["I am a Devloper", " I am a Designer", "I am a Programmer"],
-      colors = ["blue", "green", "yellow"],
-      text = document.querySelector(".text");
-
-
-    function generator() {
-      let index = 0;
-      return () => {
-        if (index >= words.length) {
-          index = 0;
-        }
-        return index++
-      }
-    }
-
-    let gen = generator();
-
-
-
-    // Typing effect
-    function typeChar(word) {
-      let i = 0;
-
-      text.innerHTML = "";
-      text.classList.add(colors[words.indexOf(word)]);
-
-
-      let id = setInterval(() => {
-        if (i >= word.length) {
-          deleteChar();
-          clearInterval(id);
-        }
-        else {
-          text.innerHTML += word[i];
-          i++;
-        }
-      }, 300);
-
-
-    }
-
-    // Deleting effect
-    function deleteChar() {
-      let word = text.innerHTML;
-      let i = word.length - 1;
-      let id = setInterval(() => {
-        if (i >= 0) {
-          text.innerHTML = text.innerHTML.substring(0, text.innerHTML.length - 1);
-          i--;
-        }
-        else {
-          text.classList.remove(colors[words.indexOf(word)]);
-          typeChar(words[gen()]);
-          clearInterval(id);
-        }
-      }, 100);
-    }
-
-    // Initializing generator
-
-    typeChar(words[gen()]);
-
-  }, [])
+  
 
   const { title } = PageDetailsSEO();
   return (
@@ -93,7 +30,6 @@ export default function Index() {
       <div className='lg:pl-16 sm:pl-3 h-[100vh] bg-base-100'>
         {/* ********************************************** */}
         <div>
-          <h1> <span className="text blue">Build</span></h1>
           {/* ***************************FOR ADMIN NOTICE *********************** */}
 
           {/* ************** WITH OUT NOTICE HOME PAGE */}
