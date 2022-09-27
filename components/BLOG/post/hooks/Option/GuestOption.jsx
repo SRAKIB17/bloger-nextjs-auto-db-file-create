074b596a2f03cfd5c5dd93ca9f4b1b0267056c77
SRAKIB17 <rakibulssc5@gmail.com>
2022-09-27 06:53:33 +0600

    'done'

--- components/BLOG/post/hooks/Option/GuestOption.jsx
+++ components/BLOG/post/hooks/Option/GuestOption.jsx
@@ -73,7 +73,7 @@ const GuestOption = ({ post }) => {
                     <button>
                         <Copy size='20' className="pr-1" /> {copyApiMsg?.copied ?
                             'Copied'
-                            : <span>Copy URL</span>}
+                            : <span>Copy API Code</span>}
                     </button>
                 </CopyToClipboard>
             </li>
